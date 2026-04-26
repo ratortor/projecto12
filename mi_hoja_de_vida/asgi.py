db51ebdfafb74701444ec87281a951881845cf56
ratortor <ratorres2097@gmail.com>
2026-04-26 10:17:40 -0500

    Add new

--- mi_hoja_de_vida/asgi.py
+++ mi_hoja_de_vida/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mi-hoja-de-vida.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mi_hoja_de_vida.settings')
 
 application = get_asgi_application()
